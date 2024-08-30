f92c18db1caa7c214e5d036189e06ff6b3767d41
zoumson <stargue49@gmail.com>
2024-08-30 16:13:04 +0800

    added comment on the process of assigning label to sample words in bayes

--- bayes.py
+++ bayes.py
@@ -9,8 +9,12 @@ class NaiveBayesClassifier:
         self.mapping = {"pos": [], "neg": []}
         self.neg_mapping = defaultdict(lambda: 0)
         self.sample_count = len(samples)
+        #record first the review classifier label
+        #all words in the given comment has the same label
         for text, label in samples:
             self.mapping[label] += self.tokenize(text)
+
+        #total of positive/negative keywords in the  comments
         self.pos_counter = Counter(self.mapping["pos"])
         self.neg_counter = Counter(self.mapping["neg"])
 
@@ -27,6 +31,8 @@ class NaiveBayesClassifier:
         pos = []
         neg = []
 
+        #calculate the frequency of each word belonging to positive or negative comment
+        #sum up the negative counterpart and the positive then compare
         for token in tokens:
             pos.append(self.pos_counter[token]/ self.sample_count)
             neg.append(self.neg_counter[token]/self.sample_count)
